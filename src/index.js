2ccb368e6d9bdfa9872d43392ebbc3fcb047590b
artemka-debug <dovgopoly123@gmail.com>
2020-08-31 14:28:10 +0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,58 +1,53 @@
-const bodyParser = require('body-parser');
+const Telegraf = require('telegraf');
 const axios = require('axios');
 const express = require('express');
 const Jimp = require('jimp');
 const fs = require('fs');
 const botToken = '1313443151:AAFyoTe-9Hr65vQcqnyFtKKDthplHOV6c8E';
 const pathToFile = `https://api.telegram.org/file/bot${botToken}`;
+const bot = new Telegraf(botToken);
 const app = express();
 
 app.use(express.static('/app'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.post('/new-message', async (req, res) => {
-    console.log('REQ', req.body);
-    const body = req.body;
-
-    if (!body.photo) {
-        await sendText(res, req, 'Expected photo');
+bot.on('message', async ctx => {
+    console.log('got message');
+    if (!ctx.updateSubTypes.includes('photo')) {
+        await ctx.reply('Expected photo');
         return;
     }
 
-//     const fileName = `${Date.now()}`;
-//     try {
-//         await downloadImage(ctx.message.photo[ctx.message.photo.length - 1].file_id, fileName, ctx);
-//     } catch (e) {
-//         await ctx.reply(JSON.stringify(e.message));
-//     }
-//
-//     await ctx.reply(`Starting converting file`);
-//     await Jimp.read(`${fileName}.jpeg`)
-//         .then(lenna => {
-//             return lenna
-//                 .resize(512, 512)
-//                 .write(`${fileName}.png`);
-//         })
-//         .catch(err => {
-//             ctx.reply(JSON.stringify(err.message));
-//         });
-//
-//     await ctx.reply(`Converted`);
-//     try {
-//         await ctx.replyWithDocument(`https://resize-photo-bot.herokuapp.com/${fileName}.png`);
-//     } catch (e) {
-//         await ctx.reply(JSON.stringify(e.message));
-//     }
-//
-//     fs.unlink(`${fileName}.png`, (err) => {
-//         console.log(err ? err : 'error is not present');
-//     });
-//     fs.unlink(`${fileName}.jpeg`, (err) => {
-//         console.log(err ? err : 'error is not present');
-//     });
+    const fileName = `${Date.now()}`;
+    try {
+        await downloadImage(ctx.message.photo[ctx.message.photo.length - 1].file_id, fileName, ctx);
+    } catch (e) {
+        await ctx.reply(JSON.stringify(e.message));
+    }
+
+    await ctx.reply(`Starting converting file`);
+    await Jimp.read(`${fileName}.jpeg`)
+        .then(lenna => {
+            return lenna
+                .resize(512, 512)
+                .write(`${fileName}.png`);
+        })
+        .catch(err => {
+            ctx.reply(JSON.stringify(err.message));
+        });
+
+    await ctx.reply(`Converted`);
+    try {
+        await ctx.replyWithDocument(`https://resize-photo-bot.herokuapp.com/${fileName}.png`);
+    } catch (e) {
+        await ctx.reply(JSON.stringify(e.message));
+    }
+
+    fs.unlink(`${fileName}.png`, (err) => {
+        console.log(err ? err : 'error is not present');
+    });
+    fs.unlink(`${fileName}.jpeg`, (err) => {
+        console.log(err ? err : 'error is not present');
+    });
 });
 
 async function downloadImage(fileInfo, fileName, ctx) {
@@ -80,29 +75,12 @@ async function downloadImage(fileInfo, fileName, ctx) {
     })
 }
 
-const sendText = async (res, req, output) => {
-    await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`,
-        {
-            chat_id: req.body.message.chat.id,
-            text: output
-        });
-    await (() => {
-        console.log('message posted');
-        res.end();
-    })
-};
-
-const sendPhoto = async (res, req, meme) => {
-    await axios.post(`https://api.telegram.org/bot${token}/sendPhoto`, {
-        chat_id: req.body.message.chat.id,
-        photo: meme
-    });
-    await (() => {
-        console.log('photo posted');
-        res.end();
-    })
-};
-
+bot.launch({
+    polling: {
+        timeout: 10,
+        limit: 1000000000000
+    }
+});
 app.listen(process.env.PORT || 8080, () => {
     console.log(`listening on port ${process.env.PORT || 8080}`);
 });
